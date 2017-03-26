35b645f512f84f4a7390c65158f89ca208e23633
北风飘然 <207426719@qq.com>
2017-03-26 19:48:06 +0800

    Update models.py

--- scantool/models.py
+++ scantool/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class httpdata(models.Model):
+    ip = models.CharField(max_length=500, verbose_name='ip')
+    title = models.CharField(max_length=500, verbose_name='title')
+    header = models.TextField(verbose_name='header')
+
+
+    def __str__(self):
+        return self.title
